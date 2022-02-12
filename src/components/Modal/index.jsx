c57dc075bd6b6f8570ac7337db295c9cf5fa34f5
Williane <williane.tads@gmail.com>
2022-02-12 20:32:39 -0300

    refactoring modal component to funtion components

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,58 +1,104 @@
-import { Component } from 'react';
-import ReactModal from 'react-modal';
+import { useState, useRef, useEffect } from "react";
+import ReactModal from "react-modal";
 
-class Modal extends Component {
-  constructor(props) {
-    super(props);
+function Modal({ isOpen, children, setIsOpen }) {
+  const [modalStatus, setModalStatus] = useState(isOpen);
+  const prevPropsRef = useRef();
 
-    const { isOpen } = this.props;
-    this.state = {
-      modalStatus: isOpen
+  useEffect(() => {
+    prevPropsRef.current = modalStatus;
+  });
+
+  const modalStatusPreviousValue = prevPropsRef.current ?? modalStatus;
+
+  useEffect(() => {
+    if (modalStatusPreviousValue !== modalStatus) {
+      setModalStatus(isOpen);
     }
-  }
+  }, [modalStatus, modalStatusPreviousValue, isOpen]);
 
-  componentDidUpdate(prevProps) {
-    const { isOpen } = this.props;
+  return (
+    <ReactModal
+      shouldCloseOnOverlayClick={!false}
+      onRequestClose={setIsOpen}
+      isOpen={modalStatus}
+      ariaHideApp={false}
+      style={{
+        content: {
+          top: "50%",
+          left: "50%",
+          right: "auto",
+          bottom: "auto",
+          marginRight: "-50%",
+          transform: "translate(-50%, -50%)",
+          background: "#F0F0F5",
+          color: "#000000",
+          borderRadius: "8px",
+          width: "736px",
+          border: "none",
+        },
+        overlay: {
+          backgroundColor: "#121214e6",
+        },
+      }}
+    >
+      {children}
+    </ReactModal>
+  );
+}
 
-    if (prevProps.isOpen !== isOpen) {
-      console.log(this.props)
-      this.setState({ modalStatus: isOpen })
-    }
-  }
+// class Modal extends Component {
+//   constructor(props) {
+//     super(props);
 
-  render() {
-    const { children, setIsOpen } = this.props;
-    const { modalStatus } = this.state;
+//     const { isOpen } = this.props;
+//     this.state = {
+//       modalStatus: isOpen
+//     }
+//   }
 
-    return (
-      <ReactModal
-        shouldCloseOnOverlayClick={!false}
-        onRequestClose={setIsOpen}
-        isOpen={modalStatus}
-        ariaHideApp={false}
-        style={{
-          content: {
-            top: '50%',
-            left: '50%',
-            right: 'auto',
-            bottom: 'auto',
-            marginRight: '-50%',
-            transform: 'translate(-50%, -50%)',
-            background: '#F0F0F5',
-            color: '#000000',
-            borderRadius: '8px',
-            width: '736px',
-            border: 'none',
-          },
-          overlay: {
-            backgroundColor: '#121214e6',
-          },
-        }}
-      >
-        {children}
-      </ReactModal>
-    );
-  }
-};
+//   componentDidUpdate(prevProps) {
+//     const { isOpen } = this.props;
+
+//     if (prevProps.isOpen !== isOpen) {
+//       console.log(this.props)
+//       this.setState({ modalStatus: isOpen })
+//     }
+//   }
+
+//   render() {
+//     const { children, setIsOpen } = this.props;
+//     const { modalStatus } = this.state;
+
+//     return (
+//       <ReactModal
+//         shouldCloseOnOverlayClick={!false}
+//         onRequestClose={setIsOpen}
+//         isOpen={modalStatus}
+//         ariaHideApp={false}
+//         style={{
+//           content: {
+//             top: '50%',
+//             left: '50%',
+//             right: 'auto',
+//             bottom: 'auto',
+//             marginRight: '-50%',
+//             transform: 'translate(-50%, -50%)',
+//             background: '#F0F0F5',
+//             color: '#000000',
+//             borderRadius: '8px',
+//             width: '736px',
+//             border: 'none',
+//           },
+//           overlay: {
+//             backgroundColor: '#121214e6',
+//           },
+//         }}
+//       >
+//         {children}
+//       </ReactModal>
+//     );
+//   }
+// };
 
 export default Modal;
